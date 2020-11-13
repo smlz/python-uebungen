c30bb69f4f60ae5c83cacf5f4e31fd07bee39cb9
Marco Schmalz <marco@schess.ch>
2020-11-13 09:48:27 +0100

    todos

---
+++ todo.py
@@ -0,0 +1,19 @@
+"""
+Ein Todo-Listen Programm
+"""
+
+print("Welcome to your todo list")
+
+todos = ...
+
+done = False
+while not done:
+    print("What would you like to do? List, Add, Remove or Quit?")
+    eingabe = input("> ").lower()
+    
+    
+    if eingabe == "quit":
+        done = True
+        
+    # FIXME: Todos auflisten, etwas hinzufügen, etwas entfernen
+    
